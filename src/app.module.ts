3e326bd5fe40110155566e225615c30b63e0f73d
selelel <janrusselgorembalem4@gmail.com>
2024-05-16 17:40:31 +0800

    WIP setting up the dev envronment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MongodbService } from './services/database/mongodb/mongodb.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    MongooseModule.forRoot(
+      `mongodb://127.0.0.1:${process.env.DB_PORT}/chitchat`,
+    ),
+    ChatModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MongodbService],
 })
 export class AppModule {}
