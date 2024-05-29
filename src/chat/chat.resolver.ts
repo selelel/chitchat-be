0f6989a8e3b0d263b5b05403d30b49c99a0aabbf
selelel <janrusselgorembalem4@gmail.com>
2024-05-29 19:43:09 +0800

    [FEAT] Entity and Auth

--- src/chat/chat.resolver.ts
+++ src/chat/chat.resolver.ts
@@ -1,28 +1,49 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { ChatService } from './chat.service';
 import { Chat } from './entities/chat.entity';
 import { UseGuards } from '@nestjs/common';
 import { GqlCurrentUser } from 'src/auth/decorator/gql.current.user';
+import { GqlAuthGuard } from 'src/auth/guards/gql.auth.guard';
+import { Message } from './entities/message.entity';
+import { GetConversation } from './dto/conversation.dto';
 
 @Resolver()
 export class ChatResolver {
   constructor(private readonly chatService: ChatService) {}
 
   @Mutation(() => Chat)
-  async createChat(@Args('recipientId') recipientId: string): Promise<Chat> {
-    const createChat = this.chatService.createPrivateRoom(
-      ' user.payload._id',
+  @UseGuards(GqlAuthGuard)
+  async createChat(
+    @Args('recipientId') recipientId: string,
+    @GqlCurrentUser() { user },
+  ): Promise<Chat> {
+    const createChat = await this.chatService.createPrivateRoom(
+      user.payload._id,
       recipientId,
     );
 
     return createChat;
   }
 
-  @Query(() => [Chat])
-  @UseGuards(GqlCurrentUser)
-  async privateChatsCreated(@GqlCurrentUser() { user }): Promise<Chat> {
-    const chat = await this.chatService.privateChats(user.payload._id);
-    console.log(chat);
-    return chat;
+  @Mutation(() => [Message])
+  @UseGuards(GqlAuthGuard)
+  async getConversation(
+    @Args('getConversation') getConversation: GetConversation,
+    @GqlCurrentUser() { user },
+  ): Promise<Message[]> {
+    try {
+      const validateUser = await this.chatService.validateUserIsOnChat({
+        userId: user.payload._id,
+        chatId: getConversation.chatId,
+      });
+
+      if (validateUser !== true) {
+        throw validateUser;
+      }
+
+      return await this.chatService.privateChats(getConversation);
+    } catch (error) {
+      return error;
+    }
   }
 }
